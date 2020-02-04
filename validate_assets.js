544f25a1833bb415bd7312c673170d07c7576219
Daniel Rigberg <daniel.rigberg@gmail.com>
2020-02-04 19:16:44 +0700

    fix validation script

--- validate_assets.js
+++ validate_assets.js
@@ -18,6 +18,8 @@ const metadataPath = path.join(assetsPath, '/metadata.json');
 const metadataString = fs.readFileSync(metadataPath);
 const data = JSON.parse(metadataString);
 for (let i = 0; i < data.comics.length; i++) {
-    const filepath = path.join(assetsPath, data.comics[i].filename);
+    const filepath = path.join(assetsPath, 'images', data.comics[i].filename);
     assert.ok(fs.existsSync(filepath), `Image ${filepath} is missing.`);
 }
+
+console.log('All is good!');
